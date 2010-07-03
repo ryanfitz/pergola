45e534e649773ed9cbf02e0661391d9159ff43dc
Ryan Fitzgerald <ryan@gymlogr.com>
2010-07-03 19:08:38 -0400

    adding new mongo controller

---
+++ app/controllers/mongo.rb
@@ -0,0 +1,30 @@
+Pergola.controllers :mongo do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :index do
+    redirect url_for :index
+  end
+  
+  get :index, :with => [:id] do
+    puts params[:id]
+    render 'mongo/index'
+  end
+  
+end
