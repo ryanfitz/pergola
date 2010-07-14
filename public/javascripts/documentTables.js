5edc956441b5037ec3f55214117d55137550587d
Ryan Fitzgerald <ryan@gymlogr.com>
2010-07-14 01:32:30 -0400

    added checkbox and styles to documents table

--- public/javascripts/documentTables.js
+++ public/javascripts/documentTables.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	$('#documents_table').dataTable();
-} );
+	$('#documents_table').dataTable({"sPaginationType": "full_numbers", "bLengthChange": false, "iDisplayLength": 50});
+	
+	$(":checkbox").click(function() {
+	  var ids = new Array();
+	  $("input:checked").each(function(index) {
+	    ids.push($(this).attr("value"));
+	    
+	    if ( $("input:checked").length -1 == index) {
+	      alert(ids.toString());
+	    }
+	  })
+  });
+  
+});
+
+    
